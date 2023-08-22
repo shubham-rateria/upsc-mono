375b5a338483389e772daaeb19e10806bc48dd25
Shubham Rateria <shubham.agarwal982@gmail.com>
2023-08-22 17:02:37 +0530

    fix: disable mobile view

--- apps/upsc-vite-app/src/components/NoMobileView/NoMobileView.tsx
+++ apps/upsc-vite-app/src/components/NoMobileView/NoMobileView.tsx
@@ -6,7 +6,7 @@ const NoMobileView = () => {
       <img src={"/img/logo.svg"} alt="Logo" />
       <p>
         We are building the mobile version of the platform. Please view this on
-        your desktop / laptop for now.
+        your desktop or in landscape mode of your iPad / Tablet.
       </p>
       <p>Apologies for any inconvenience.</p>
     </div>
