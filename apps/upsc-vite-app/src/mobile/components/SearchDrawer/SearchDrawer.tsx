b6c5d6b72246e1fd20b229563d4a239f1ceaae23
Shubham Rateria <shubham.agarwal982@gmail.com>
2023-08-31 16:05:41 +0530

    fix: build

--- apps/upsc-vite-app/src/mobile/components/SearchDrawer/SearchDrawer.tsx
+++ apps/upsc-vite-app/src/mobile/components/SearchDrawer/SearchDrawer.tsx
@@ -18,28 +18,6 @@ type Props = {
   onClose: () => void;
 };
 
-function flattenObject(obj: any) {
-  const result: any[] = [];
-
-  function extractLeafValues(obj: any) {
-    if (typeof obj === "object" && obj !== null) {
-      if (Array.isArray(obj)) {
-        obj.forEach((item) => extractLeafValues(item));
-      } else {
-        for (const key in obj) {
-          result.push(key);
-          extractLeafValues(obj[key]);
-        }
-      }
-    } else {
-      result.push(obj);
-    }
-  }
-
-  extractLeafValues(obj);
-  return result;
-}
-
 const SearchDrawer: FC<Props> = ({ isOpen, onClose }) => {
   const searchParamsClass = useContext(SearchParamsContext);
   const [selectedL0, setSelectedL0] = useState("");
