676f233a687bdcc9fa613f0badc7cc810dce352a
Shubham Rateria <shubham.agarwal982@gmail.com>
2024-02-12 13:35:39 +0530

    feat: do spaces

--- servers/backend/services/s3/index.ts
+++ servers/backend/services/s3/index.ts
@@ -1,24 +1,36 @@
-import AWS from 'aws-sdk';
+import AWS from "aws-sdk";
 
 // Set your AWS credentials and region
-AWS.config.update({
-  accessKeyId: 'AKIATNMSO37KTJZYLPCJ',
-  secretAccessKey: 'OywlmVBlGs5JI+kBKwL5Jjy/2s6Ml4/c6J4A7n3E',
-  region: 'ap-south-1'
-});
+// AWS.config.update({
+//   accessKeyId: 'AKIATNMSO37KTJZYLPCJ',
+//   secretAccessKey: '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',
+//   region: 'ap-south-1'
+// });
 
 // Function to get a signed URL for an S3 object
-export async function getSignedUrl(bucketName: string, objectKey: string, expiresInMinutes: number) {
-  const s3 = new AWS.S3();
+export async function getSignedUrl(
+  bucketName: string,
+  objectKey: string,
+  expiresInMinutes: number
+) {
+  const s3 = new AWS.S3({
+    s3ForcePathStyle: false, // Configures to use subdomain/virtual calling format.
+    endpoint: "https://blr1.digitaloceanspaces.com",
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: process.env.SPACES_KEY || "",
+      secretAccessKey: process.env.SPACES_SECRET || "",
+    },
+  });
 
   const params = {
     Bucket: bucketName,
     Key: objectKey,
-    Expires: expiresInMinutes * 60 // Convert minutes to seconds
+    Expires: expiresInMinutes * 60, // Convert minutes to seconds
   };
 
   return new Promise((resolve, reject) => {
-    s3.getSignedUrl('getObject', params, (err: any, url: string) => {
+    s3.getSignedUrl("getObject", params, (err: any, url: string) => {
       if (err) {
         reject(err);
       } else {
