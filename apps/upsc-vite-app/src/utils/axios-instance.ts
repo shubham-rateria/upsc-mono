89f09cf6c2573a908c9138b0956351a7ccabd39d
Shubham Rateria <shubham.agarwal982@gmail.com>
2024-02-14 16:31:11 +0530

    feat: azure

--- apps/upsc-vite-app/src/utils/axios-instance.ts
+++ apps/upsc-vite-app/src/utils/axios-instance.ts
@@ -5,8 +5,8 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://api.upscsmartnotes.com"
-      : "http://localhost:3000",
+      ? "http://4.240.106.5:3000"
+      : "http://4.240.106.5:3000",
   // baseURL: "http://localhost:3000",
   timeout: 120000,
 });
